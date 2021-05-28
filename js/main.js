e08cc25c9180fa56fb031afc6a9b490867c70fc5
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2021-05-29 09:02:49 +1000

    Feat: fix section

--- js/main.js
+++ js/main.js
@@ -400,6 +400,49 @@
 		}
 	};
 
+	var doneAnimate = function() {
+		var done = $('#fh5co-done');
+		if ( done.length > 0 ) {	
+
+			done.waypoint( function( direction ) {
+										
+				if( direction === 'down' && !$(this.element).hasClass('animated') ) {
+
+
+					var sec = done.find('.to-animate').length,
+						sec = parseInt((sec * 200) + 400);
+
+					setTimeout(function() {
+						done.find('.to-animate').each(function( k ) {
+							var el = $(this);
+							
+							setTimeout ( function () {
+								el.addClass('fadeInUp animated');
+							},  k * 200, 'easeInOutExpo' );
+							
+						});
+					}, 200);
+
+					setTimeout(function() {
+						services.find('.to-animate-2').each(function( k ) {
+							var el = $(this);
+							
+							setTimeout ( function () {
+								el.addClass('bounceIn animated');
+							},  k * 200, 'easeInOutExpo' );
+							
+						});
+					}, sec);
+
+					
+					$(this.element).addClass('animated');
+						
+				}
+			} , { offset: '80%' } );
+
+		}
+	};
+
 
 	var teamAnimate = function() {
 		var team = $('#fh5co-team');
@@ -508,12 +551,12 @@
 		legalAnimate();
 		gettingStartedAnimate();
 		pricingAnimate();
+		doneAnimate();
 		servicesAnimate();
 		teamAnimate();
 		footerAnimate();
 		counter();
 		counterWayPoint();
-
 	});
 
 
