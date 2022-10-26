59b4cdec291e82faa2dd62bc737d35a6f48305f0
Justin Lanyon-Owen <jlanyon-owen@outlook.com>
2022-10-26 10:40:50 +1000

    DW-32 remove redundant instructions

--- pgp/gen-key.sh
+++
@@ -1,8 +0,0 @@
-echo First name
-read FIRST
-echo Last name
-read LAST
-
-gpg --gen-key
-gpg --export -a $FIRST $LAST > $FIRST.$LAST@dltx.io.asc
-gpg --armor --export-secret-key $FIRST.$LAST@dltx.io
